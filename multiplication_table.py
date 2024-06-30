501665600f4ee4a61194758e0d9026965c2ad57f
Mangesh Korde <143051332+mangesh-korde@users.noreply.github.com>
2024-06-30 12:24:10 +0530

    Add files via upload

---
+++ multiplication_table.py
@@ -0,0 +1,8 @@
+# Q..create Multiplication Table...
+
+n=input("Enter Number:")
+n=int(n)
+print("Multiplication Table:")
+for i in range(1,11):
+    print(n,"X",i,"=",(n*i))
+   # print("\n")..It Takes more SPace..

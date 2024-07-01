5dfd8f94cbb264ac0cf8466df6354d4d1b03f640
Mangesh Korde <143051332+mangesh-korde@users.noreply.github.com>
2024-07-01 13:15:30 +0530

    Add files via upload

---
+++ random_no_generate.py
@@ -0,0 +1,19 @@
+#Q.. Python program to generate random number??
+
+#Method 1
+
+import random
+n=random.randint(50,100)
+print("Random no=",n)
+#Method 2
+'''
+from random import *
+n=randint(100,200)
+print("Random no=",n)
+'''
+#Method 3
+'''
+import random as r
+n=r.randint(20,40)
+print("Random no=",n)
+'''

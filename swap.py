3d9eeaee3280e70b934709ebf60062f9e6bee12b
Mangesh Korde <143051332+mangesh-korde@users.noreply.github.com>
2024-07-02 15:53:20 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,10 @@
+#Q..python program to swap two variables..
+
+a=int(input("Enter First no:"))
+b=int(input("ENter Second no:"))
+t=a
+a=b
+b=t
+print("After Swapping:")
+print("value of a=",a)
+print("value of  b=",b)
